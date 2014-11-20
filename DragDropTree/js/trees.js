f5455171f51b96abc4c56d74ad35ba67df6fa2c8
kim ji hoon <whitexozu@gmail.com>
2014-11-20 17:52:46 +0900

    first commit

---
+++ DragDropTree/js/trees.js
@@ -0,0 +1,82 @@
+(function() {
+	'use strict';
+
+	angular.module('treesApp', [ 'ui.tree' ]).controller('treesCtrl', function($scope, keys) {
+		$scope.parameters = {
+			dragEnabled : true,
+			emptyPlaceholderEnabled : false,
+			maxDepth : 2,
+			dragDelay : 0,
+			dragDistance : 0,
+			lockX : false,
+			lockY : false,
+			boundTo : '',
+			spacing : 20,
+			coverage : 50,
+			cancelKey : 'esc',
+			copyKey : 'shift',
+			selectKey : 'ctrl',
+			enableExpandOnHover : true,
+			expandOnHover : 500,
+		};
+		
+		$scope.keys = keys;
+		
+		$scope.alert = function(scope) {
+			console.log(scope);
+		};
+
+		$scope.remove = function(scope) {
+			scope.remove();
+		};
+
+		$scope.toggle = function(scope) {
+			scope.toggle();
+		};
+
+		$scope.newSubItem = function(scope) {
+			var nodeData = scope.$modelValue;
+			nodeData.nodes.push({
+				id : nodeData.id * 10 + nodeData.nodes.length,
+				title : nodeData.title + '.' + (nodeData.nodes.length + 1),
+				nodes : []
+			});
+		};
+
+		$scope.tree1 = [ {
+			"id" : 1,
+			"title" : "tree1 - item1",
+			"nodes" : [],
+		}, {
+			"id" : 2,
+			"title" : "tree1 - item2",
+			"nodes" : [],
+		}, {
+			"id" : 3,
+			"title" : "tree1 - item3",
+			"nodes" : [],
+		}, {
+			"id" : 4,
+			"title" : "tree1 - item4",
+			"nodes" : [],
+		} ];
+		$scope.tree2 = [ {
+			"id" : 1,
+			"title" : "tree2 - item1",
+			"nodes" : [],
+		}, {
+			"id" : 2,
+			"title" : "tree2 - item2",
+			"nodes" : [],
+		}, {
+			"id" : 3,
+			"title" : "tree2 - item3",
+			"nodes" : [],
+		}, {
+			"id" : 4,
+			"title" : "tree2 - item4",
+			"nodes" : [],
+		} ];
+	});
+
+})();
